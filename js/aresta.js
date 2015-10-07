9becf38bae022de4d856ee99ed80a51f719e9f87
Welton Lima <welton.s92@gmail.com>
2015-10-07 09:30:58 -0300

    Gerar Arestas e Vertices
    Na tela de index.html ja da pra inserir os vertices e as arestas. Esta
    setando tudo direito.

--- js/aresta.js
+++ js/aresta.js
@@ -2,8 +2,8 @@ function Aresta(vertice_1, vertice_2){
 	
 	var aresta = {};
 	
-	aresta.verticeInicio;
-	aresta.verticeFim;
+	aresta.verticeInicio =  new Vertice();
+	aresta.verticeFim =  new Vertice();
 	
 	
 	aresta.criarAresta = function(vertice_1, vertice_2){
@@ -27,8 +27,44 @@ function Aresta(vertice_1, vertice_2){
 	aresta.getVerticeFim = function(){
 		return aresta.verticeFim;	
 	};
+
+    aresta.getCoordenadas = function () {
+
+        var aCoo = [
+            [aresta.verticeInicio.x,aresta.verticeInicio.y],
+            [aresta.verticeFim.x,aresta.verticeFim.y]
+        ];
+
+        return aCoo;
+    };
+
+    aresta.desenharAresta = function (id) {
+
+        var aCoo = aresta.getCoordenadas();
+        // GERAR ARTESTA
+        var c = document.getElementById(id);
+        var ctx = c.getContext("2d");
+        // coordenada (x,y)
+        ctx.moveTo(aCoo[0][0],aCoo[0][1]);
+        ctx.lineTo(aCoo[1][0],aCoo[1][1]);
+        ctx.stroke();
+        /*
+        // GERAR VERTICE
+        var ctx = c.getContext("2d");
+        ctx.beginPath();
+        // x, y, diamentro, circuferencia,
+        ctx.arc(aCoo[0][0],aCoo[0][1],5,0,2*Math.PI);
+        ctx.stroke();
+        // GERAR VERTICE
+        var ctx = c.getContext("2d");
+        ctx.beginPath();
+        // x, y, diamentro, circuferencia,
+        ctx.arc(aCoo[1][0],aCoo[1][1],5,0,2*Math.PI);
+        ctx.stroke();
+        */
+    };
 	
-	aresta.criarAresta(vertice_1, vertice_2);
+	//aresta.criarAresta(vertice_1, vertice_2);
 	
 	return aresta;
 }
--- js/vertice.js
+++ js/vertice.js
@@ -8,6 +8,8 @@ function Vertice(){
 	vertice.ativo;
 	vertice.arestas = [];
 	vertice.grau = 0;
+    vertice.x = 0;
+    vertice.y = 0;
 	
 	vertice.criarVertice = function(){
 		vertice.id = ID_VERTICES;
@@ -16,7 +18,8 @@ function Vertice(){
 	};
 	
 	vertice.setCoordenadasNaTela = function(x, y){
-		
+        vertice.x = x;
+        vertice.y = y;
 	};
 	
 	vertice.addAresta = function(aresta){
@@ -36,6 +39,16 @@ function Vertice(){
 	vertice.getAresta = function(index){
 		return vertice.arestas[index];	
 	};
+
+    vertice.desenharVertice = function (id) {
+        // GERAR VERTICE
+        var c = document.getElementById(id);
+        var ctx = c.getContext("2d");
+        ctx.beginPath();
+        // x, y, diamentro, circuferencia,
+        ctx.arc(vertice.x,vertice.y,5,0,2*Math.PI);
+        ctx.stroke();
+    };
 	
 	vertice.criarVertice();
 	
